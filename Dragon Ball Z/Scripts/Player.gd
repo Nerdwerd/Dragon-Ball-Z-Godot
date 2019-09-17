81bc3a62e2cc32759f843b5f249799bc0d7663b3
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2019-09-17 13:52:54 -0500

    Movement Finished

--- Dragon Ball Z/Scripts/Player.gd
+++ Dragon Ball Z/Scripts/Player.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 
 #Variables
 export var pspeed = 400
-export var gravity = 200
-export var pjump = -3000
+export var gravity = 100
+export var pjump = -2000
 var motion = Vector2()
 var GROUND = Vector2(0,-1)
 
@@ -17,13 +17,16 @@ func _process(delta):
 		$AnimatedSprite.set_animation("Jump")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -pspeed
-		$AnimatedSprite.set_animation("DashForward")
+		if is_on_floor():
+			$AnimatedSprite.set_animation("DashBack")
 	elif Input.is_action_pressed("ui_right"):
 		motion.x = pspeed
-		
+		if is_on_floor():
+			$AnimatedSprite.set_animation("DashForward")	
 	else:
 		motion.x = 0
 		if is_on_floor():
 			motion.y = 0
+			$AnimatedSprite.set_animation("Idle")
 
 	motion = move_and_slide(motion, GROUND)
