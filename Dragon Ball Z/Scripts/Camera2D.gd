b0283c9458616242577957f6b39437b740d1a271
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2019-09-17 14:44:02 -0500

    Started Work on Camera

---
+++ Dragon Ball Z/Scripts/Camera2D.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	VisualServer.set_default_clear_color(Color(0.0,0.0,0.0,1.0))
+	pass 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
